53175b4b4d28bb50ac255c55df06adb35d5b99ec
Ana.Laura.Gonzalez <115861528+ALGzz@users.noreply.github.com>
2022-12-20 04:33:34 -0300

    Add files via upload

---
+++ js/javascrip.js
@@ -0,0 +1,43 @@
+
+function cambiarfondo1() {
+    document.body.style.backgroundColor = "#000066";
+    document.getElementById("titulos").style.backgroundColor= "#000066";
+    document.getElementById("titulos").style.color= "#FDEBD0";
+    document.getElementById("titulo1").style.color= "#FDEBD0";
+    document.getElementById("titulo2").style.color= "#FDEBD0";
+    document.getElementById("titulo3").style.color= "#FDEBD0";
+    document.getElementById("titulo4").style.color= "#FDEBD0";
+    document.getElementById("titulo5").style.color= "#FDEBD0";
+    document.getElementById("titulo6").style.color= "#FDEBD0";
+    document.getElementById("titulo7").style.color= "#FDEBD0";
+    document.getElementById("titulo8").style.color= "#FDEBD0";
+    document.getElementById("titulo8").style.backgroundColor= "#510e88";
+    document.getElementById("titulo9").style.color= "#FDEBD0";
+    document.getElementById("titulo9").style.backgroundColor= "#510e88";
+    document.getElementById("titulo10").style.color= "#FDEBD0";
+    document.getElementById("titulo10").style.backgroundColor= "#510e88";
+    document.getElementById("titulo11").style.color= "#FDEBD0";
+    document.getElementById("titulo11").style.backgroundColor= "#510e88";
+}
+function cambiarfondo2() {
+    document.body.style.backgroundColor = "goldenrod";
+    document.getElementById("titulos").style.backgroundColor= "goldenrod";
+    document.getElementById("titulos").style.color= "#3949AB";
+    document.getElementById("titulo1").style.color= "#880E4F";
+    document.getElementById("titulo2").style.color= "#3949AB";
+    document.getElementById("titulo3").style.color= "#3949AB";
+    document.getElementById("titulo4").style.color= "#3949AB";
+    document.getElementById("titulo5").style.color= "#3949AB";
+    document.getElementById("titulo6").style.color= "#3949AB";
+    document.getElementById("titulo7").style.color= "#880E4F";
+    document.getElementById("titulo8").style.color= "black";
+    document.getElementById("titulo8").style.backgroundColor= "lightgoldenrodyellow";
+    document.getElementById("titulo9").style.color= "black";
+    document.getElementById("titulo9").style.backgroundColor= "lightgoldenrodyellow";
+    document.getElementById("titulo10").style.color= "black";
+    document.getElementById("titulo10").style.backgroundColor= "lightgoldenrodyellow";
+    document.getElementById("titulo11").style.color= "black";
+    document.getElementById("titulo11").style.backgroundColor= "lightgoldenrodyellow";
+  }
+function cartel() {alert("Ana Laura González - Desarrolladora y Diseñadora Web - Rosario / Santa Fe")}
+
